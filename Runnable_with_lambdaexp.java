3427210efc62f0a21b2f097504571aad543e175d
Nikhil-Sanjay <nikhilsanjay421@gmail.com>
2025-05-13 11:06:32 +0530

    Update Runnable_with_lambdaexp.java

--- Runnable_with_lambdaexp.java
+++ Runnable_with_lambdaexp.java
@@ -22,27 +22,5 @@ class Runnable_with_lambdaexp {
     }
 }
 
-class Multithreading_using_runnable {
-    public static void main(String[] args) {
-        Runnable a=()->{
-            for(int i=0;i<10;i++){
-                try{Thread.sleep(1000);}catch(Exception e){}
-                System.out.println("In A");
-            }
-            
-        };
-        Runnable b=()->{
-            for(int i=0;i<20;i++){
-                try{Thread.sleep(1000);}catch(Exception e){}
-                System.out.println("In B");
-            }
-            
-        };
-        Thread t1=new Thread(a);
-        Thread t2=new Thread(b);
-        t1.start();
-        try{Thread.sleep(1000);}catch(Exception e){}
-        t2.start();
-    }
-}
+
 
